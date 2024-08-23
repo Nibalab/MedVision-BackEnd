9f827ec0e16882e3cfe9f6b0fa5d667ef18897a2
Nibalab <abdalbakinibal@gmail.com>
2024-08-23 13:37:48 +0300

    adding the schema for the admin

--- MedVision-BackEnd/database/migrations/2024_08_23_102213_create_admin_logs_table.php
+++ MedVision-BackEnd/database/migrations/2024_08_23_102213_create_admin_logs_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('admin_logs', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
             $table->foreignId('admin_id')->constrained('users')->onDelete('cascade');
-            $table->string('action');
+            $table->string('action'); 
             $table->timestamps();
         });
     }
