0848d56c4e049a0543cff59bc5e757860e2ed5ba
Nibalab <abdalbakinibal@gmail.com>
2024-08-23 14:15:42 +0300

    defining relationships in Models for Appointment

--- MedVision-BackEnd/app/Models/Appointment.php
+++ MedVision-BackEnd/app/Models/Appointment.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    use HasFactory;
+    protected $fillable = ['patient_id', 'doctor_id', 'appointment_date', 'status'];
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
 }
