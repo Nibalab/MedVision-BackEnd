8ed0d5cfd1009b4fd273a9ba196193df4f4219ca
Nibalab <abdalbakinibal@gmail.com>
2024-08-23 14:09:52 +0300

    defining relationships in Models for users and CtScans

--- MedVision-BackEnd/app/Models/CtScan.php
+++ MedVision-BackEnd/app/Models/CtScan.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class CtScan extends Model
 {
-    use HasFactory;
+    protected $fillable = ['doctor_id', 'patient_id', 'file_path'];
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
+
+    public function threeDModel()
+    {
+        return $this->hasOne(ThreeDModel::class);
+    }
+
+    public function report()
+    {
+        return $this->hasOne(Report::class);
+    }
 }
