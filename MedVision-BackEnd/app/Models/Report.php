8980b919e5c2b39660ec9627056e54bde56f6c15
Nibalab <abdalbakinibal@gmail.com>
2024-08-23 14:14:20 +0300

    defining relationships in Models for Reports

--- MedVision-BackEnd/app/Models/Report.php
+++ MedVision-BackEnd/app/Models/Report.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    use HasFactory;
+    protected $fillable = ['ct_scan_id', 'doctor_id', 'patient_id', 'report_content', 'status'];
+
+    public function ctScan()
+    {
+        return $this->belongsTo(CtScan::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, 'patient_id');
+    }
 }
