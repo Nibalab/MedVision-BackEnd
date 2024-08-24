a63d1e8326ab65d940470f9862f091d6ae261c4a
Nibalab <abdalbakinibal@gmail.com>
2024-08-25 01:06:34 +0300

    updating MessageSeeder

--- MedVision-BackEnd/database/seeders/MessageSeeder.php
+++ MedVision-BackEnd/database/seeders/MessageSeeder.php
@@ -2,16 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Message;
 use Illuminate\Database\Seeder;
 
 class MessageSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Message::factory()->count(20)->create();
     }
 }
+
