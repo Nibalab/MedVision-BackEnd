6d2eead5f921ae760f4b73f8fe8bfd2af015b0b1
Nibalab <abdalbakinibal@gmail.com>
2024-08-25 00:51:03 +0300

    updating AdminLogFactory

--- MedVision-BackEnd/database/factories/AdminLogFactory.php
+++ MedVision-BackEnd/database/factories/AdminLogFactory.php
@@ -2,22 +2,19 @@
 
 namespace Database\Factories;
 
+use App\Models\AdminLog;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AdminLog>
- */
 class AdminLogFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = AdminLog::class;
+
+    public function definition()
     {
         return [
-            //
+            'admin_id' => User::factory()->create(['role' => 'admin'])->id,
+            'action' => $this->faker->sentence,
         ];
     }
 }
