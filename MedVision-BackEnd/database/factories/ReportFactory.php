5235ca320ee099157d6fd5ecf247ccd748854c5b
Nibalab <abdalbakinibal@gmail.com>
2024-08-25 00:45:49 +0300

    updating ReportFactory

--- MedVision-BackEnd/database/factories/ReportFactory.php
+++ MedVision-BackEnd/database/factories/ReportFactory.php
@@ -2,22 +2,24 @@
 
 namespace Database\Factories;
 
+use App\Models\CtScan;
+use App\Models\Report;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Report>
- */
 class ReportFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Report::class;
+
+    public function definition()
     {
         return [
-            //
+            'ct_scan_id' => CtScan::factory(),
+            'doctor_id' => User::factory()->create(['role' => 'doctor'])->id,
+            'patient_id' => User::factory()->create(['role' => 'patient'])->id,
+            'report_content' => $this->faker->paragraph,
+            'status' => $this->faker->randomElement(['draft', 'finalized']),
         ];
     }
 }
+
