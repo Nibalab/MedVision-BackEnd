93911eb40692cecf0099a35930b53a2a4b62c34a
Nibalab <abdalbakinibal@gmail.com>
2024-08-25 00:39:15 +0300

    updating CtScanFactory

--- MedVision-BackEnd/database/factories/CtScanFactory.php
+++ MedVision-BackEnd/database/factories/CtScanFactory.php
@@ -2,22 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\CtScan;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CtScan>
- */
 class CtScanFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = CtScan::class;
+
+    public function definition()
     {
         return [
-            //
+            'doctor_id' => User::factory()->create(['role' => 'doctor'])->id,
+            'patient_id' => User::factory()->create(['role' => 'patient'])->id,
+            'file_path' => $this->faker->filePath(),
         ];
     }
 }
+
