7d28b063ea5a77c8f35b05860d2f602d4566b675
Nibalab <abdalbakinibal@gmail.com>
2024-08-25 00:53:21 +0300

    updating USerSeeder

--- MedVision-BackEnd/database/seeders/UserSeeder.php
+++ MedVision-BackEnd/database/seeders/UserSeeder.php
@@ -2,16 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        User::factory()->count(10)->create();
     }
 }
+
