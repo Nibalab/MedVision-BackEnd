f186f04ccc71e96d9fd6d5e8233df331545952b1
Nibalab <abdalbakinibal@gmail.com>
2024-08-25 00:55:55 +0300

    adding ThreeDModelSeeder

---
+++ MedVision-BackEnd/database/seeders/ThreeDModelSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ThreeDModelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
