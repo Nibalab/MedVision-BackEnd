f28b2567efa6be758fd2598879ffde388560c397
Nibalab <abdalbakinibal@gmail.com>
2024-08-25 00:55:02 +0300

    updating CtScanSeeder

--- MedVision-BackEnd/database/seeders/CtScanSeeder.php
+++ MedVision-BackEnd/database/seeders/CtScanSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\CtScan;
 use Illuminate\Database\Seeder;
 
 class CtScanSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        CtScan::factory()->count(10)->create();
     }
 }
