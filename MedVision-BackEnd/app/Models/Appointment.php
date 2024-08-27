f12237f67501fd75b02125b0ce77fc0b07f97728
Nibalab <abdalbakinibal@gmail.com>
2024-08-27 22:58:50 +0300

    updating Appointment model

--- MedVision-BackEnd/app/Models/Appointment.php
+++ MedVision-BackEnd/app/Models/Appointment.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -13,7 +12,7 @@ class Appointment extends Model
 
     public function doctor()
     {
-        return $this->belongsTo(User::class, 'doctor_id');
+        return $this->belongsTo(Doctor::class, 'doctor_id');
     }
 
     public function patient()
