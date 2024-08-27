b6f21b91785129b1b8646ddb73216d494209e4d9
Nibalab <abdalbakinibal@gmail.com>
2024-08-27 21:31:30 +0300

    adding Doctor doctor in DatabseSeeder

--- MedVision-BackEnd/database/seeders/DatabaseSeeder.php
+++ MedVision-BackEnd/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             AppointmentSeeder::class,
             MessageSeeder::class,
             AdminLogSeeder::class,
+            DoctorSeeder::class,
         ]);
     }
 }
