b1215ee0c4822789e4719c3b811c69fab5e2998e
Nibalab <abdalbakinibal@gmail.com>
2024-09-07 21:07:34 +0300

    updating the bootsrap.js to add the socket

--- MedVision-BackEnd/resources/js/bootstrap.js
+++ MedVision-BackEnd/resources/js/bootstrap.js
@@ -1,4 +1,16 @@
+// Import Axios
 import axios from 'axios';
 window.axios = axios;
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Import Echo and Socket.IO
+import Echo from 'laravel-echo';
+import io from 'socket.io-client';
+
+// This assumes your server.js is running on port 3000
+window.io = io;
+
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':3000'  // or specify the exact host 'http://localhost:3000'
+});
